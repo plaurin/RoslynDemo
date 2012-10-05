2c864c7b6bdd9935c8941a307934ade08c0862d0
Pascal Laurin <plaurin@hotmail.com>
2012-10-05 19:01:25 -0400

    Initial commit

---
+++ SyntaxAnalysis/Node.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roslyn.Compilers.CSharp;
+
+namespace SyntaxAnalysis
+{
+	internal class DgmlNode
+	{
+		private List<DgmlNode> childNodes;
+
+		public DgmlNode(string label, string category, string kind, string span)
+		{
+			this.Label = label;
+			this.Category = category;
+			this.Kind = kind;
+			this.Span = span;
+			this.childNodes = new List<DgmlNode>();
+		}
+
+		public string Label { get; private set; }
+		public string Category { get; private set; }
+		public string Kind { get; private set; }
+		public string Span { get; private set; }
+
+		public IEnumerable<DgmlNode> Childs
+		{
+			get { return this.childNodes; }
+		}
+
+		public override string ToString()
+		{
+			if (this.Label == this.Kind)
+				return string.Format("{0} {1} - {2}", this.Label, this.Span, this.Category);
+			else
+				return string.Format("{0} {1} - {2}: {3}", this.Kind, this.Span, this.Category, this.Label);
+		}
+
+		public static DgmlNode Create(SyntaxNode node)
+		{
+			return new DgmlNode(node.Kind.ToString(), "SyntaxNode", node.Kind.ToString(), node.Span.ToString());
+		}
+
+		public static DgmlNode Create(SyntaxToken token)
+		{
+			return new DgmlNode(token.ToString(), "SyntaxToken", token.Kind.ToString(), token.Span.ToString());
+		}
+
+		public static DgmlNode Create(SyntaxTrivia trivia)
+		{
+			return new DgmlNode(trivia.Kind.ToString(), "SyntaxTrivia", trivia.Kind.ToString(), trivia.Span.ToString());
+		}
+
+		public void AddChild(DgmlNode childNode)
+		{
+			this.childNodes.Add(childNode);
+		}
+	}
+}
---
+++ SyntaxAnalysis/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Roslyn.Compilers.CSharp;
+
+namespace SyntaxAnalysis
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SyntaxTree tree = SyntaxTree.ParseText(
+@"using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+ 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }
+}");
+
+			var root = (CompilationUnitSyntax)tree.GetRoot();
+			var dgmlNode = ProcessNode(root);
+
+			Console.ReadLine();
+
+			WriteAndOpenDgml(dgmlNode);
+		}
+
+		private static DgmlNode ProcessNode(SyntaxNode node, int indent = 0)
+		{
+			var dgmlNode = DgmlNode.Create(node);
+			//Console.WriteLine(string.Join("", Enumerable.Repeat(" ", indent)) + dgmlNode);
+			PrintNode(dgmlNode, indent);
+
+			foreach (var childNodeOrToken in node.ChildNodesAndTokens())
+			{
+				if (childNodeOrToken.IsNode)
+					dgmlNode.AddChild(ProcessNode((SyntaxNode)childNodeOrToken, indent + 1));
+				else if (childNodeOrToken.IsToken)
+					dgmlNode.AddChild(ProcessToken((SyntaxToken)childNodeOrToken, indent + 1));
+			}
+
+			return dgmlNode;
+		}
+
+		private static DgmlNode ProcessToken(SyntaxToken token, int indent = 0)
+		{
+			var dgmlNode = DgmlNode.Create(token);
+			//Console.WriteLine(string.Join("", Enumerable.Repeat(" ", indent)) + dgmlNode);
+			PrintNode(dgmlNode, indent);
+
+			foreach (var trivia in token.GetAllTrivia())
+			{
+				dgmlNode.AddChild(ProcessTrivia(trivia, indent + 1));
+			}
+
+			return dgmlNode;
+		}
+
+		private static DgmlNode ProcessTrivia(SyntaxTrivia trivia, int indent = 0)
+		{
+			var dgmlNode = DgmlNode.Create(trivia);
+			//Console.WriteLine(string.Join("", Enumerable.Repeat(" ", indent)) + dgmlNode);
+			PrintNode(dgmlNode, indent);
+
+			return dgmlNode;
+		}
+
+		private static void PrintNode(DgmlNode dgmlNode, int indent)
+		{
+			Console.WriteLine(string.Join("", Enumerable.Repeat(" ", indent)) + dgmlNode);
+		}
+
+		private static void WriteAndOpenDgml(DgmlNode dgmlNodes)
+		{
+			//var fileName = Path.ChangeExtension(Path.GetTempFileName(), "dgml");
+			var fileName = @"..\..\syntax.dgml";
+
+			var document = new XDocument();
+			XNamespace ns = "http://schemas.microsoft.com/vs/2009/dgml";
+
+			document.Add(new XElement(ns + "DirectedGraph",
+				new XElement(ns + "Nodes",
+					new XElement(ns + "Node", 
+						new XAttribute("Id", "Toto"))),
+				new XElement(ns + "Links")));
+
+			document.Save(fileName);
+		}
+	}
+}
